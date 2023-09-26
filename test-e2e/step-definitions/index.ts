06922bbc43541eb8fc713422f3333b50e99f1bca
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-09-26 15:13:46 +0300

    fixed dry-run issue

--- test-e2e/step-definitions/index.ts
+++ test-e2e/step-definitions/index.ts
@@ -1,6 +1,6 @@
 import { Then, When } from '@cucumber/cucumber';
 import memory from '@qavajs/memory';
-import { expect } from 'chai';
+import assert from 'node:assert';
 
 When('I set memory value {string} as {string}', function (key, value) {
     memory.setValue(key, value);
@@ -9,13 +9,13 @@ When('I set memory value {string} as {string}', function (key, value) {
 Then('I expect {string} memory value to be equal {string}', function(actual, expected) {
     const actualValue = memory.getValue(actual);
     const expectedValue = memory.getValue(expected);
-    expect(expectedValue).to.eql(actualValue);
+    assert.equal(expectedValue, actualValue);
 });
 
 Then('I expect {string} memory value to be equal:', function(actual, multilineExpected) {
     const actualValue = memory.getValue(actual);
     const expectedValue = memory.getValue(multilineExpected);
-    expect(expectedValue).to.eql(actualValue);
+    assert.equal(expectedValue, actualValue);
 });
 
 When('I throw {string} error', function (errorMessage) {
