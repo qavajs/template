5d737d45bf2af3fbe735ac40a30bbc89696b3fed
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-09-26 15:28:07 +0300

    fixed package-lock.json

--- vitest.config.js
+++ vitest.config.js
@@ -3,9 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'c8',
-      include: ["src/**/*.ts"],
-      exclude: ["/lib/", "/node_modules/"],
+      provider: 'v8',
+      include: ['src/**/*.ts'],
       branches: 80,
       functions: 90,
       lines: 90,
